f603a59d7003e7c624806bdbcece8da91c15a98b
nick8319 <zhiltsov.nick@gmail.com>
2024-03-24 16:55:53 +0100

    feat: update naming

--- src/pages/share/safe-app.tsx
+++ src/pages/share/safe-app.tsx
@@ -11,7 +11,7 @@ const ShareSafeApp = () => {
   return (
     <>
       <Head>
-        <title>{`Safe{Wallet} – Safe Apps`}</title>
+        <title>{`Oasis Safe – Safe Apps`}</title>
       </Head>
 
       <main>
--- src/pages/welcome/accounts.tsx
+++ src/pages/welcome/accounts.tsx
@@ -6,7 +6,7 @@ const Accounts: NextPage = () => {
   return (
     <>
       <Head>
-        <title>{'Safe{Wallet} – My accounts'}</title>
+        <title>{'Oasis Safe – My accounts'}</title>
       </Head>
 
       <MyAccounts />
